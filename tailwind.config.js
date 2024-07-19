06e608c480c930437954651bff0752bd80a01ff5
Kafui Mortey <morteykafui@gmail.com>
2024-07-19 22:13:24 +0000

    finish project

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        darkColor: "#171717",
+        lightDarkColor: "#212121",
+        redColor: "#D35A5A",
+        yellowColor: "#C2AA3C",
+        pinkColor: "#B17BB6",
+        lightBlueColor: "#5B99AC",
+      },
+    },
+  },
+  plugins: [],
+};
